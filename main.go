4c13a6c62190a3aae743b9cf1b8a9a25b98a9eec
Elihu Cruz <eaca230596@gmail.com>
2022-06-10 20:01:49 -0500

    chore: update init configuration

--- main.go
+++ main.go
@@ -2,23 +2,16 @@ package main
 
 import (
 	"fmt"
-	"net/http"
-
-	"github.com/gorilla/mux"
-	// "github.com/ea2305/go-gards/main"
 )
 
 func main() {
-	r := mux.NewRouter()
-	var addr = ":8080"
 	app := App{}
-	app.initRoutes(r)
-
-	server := &http.Server{
-		Addr:    ":8080",
-		Handler: r,
+	var config = AppConfig{
+		Addr: ":8080",
 	}
 
-	fmt.Printf("[Starting server in localhost%v...]", addr)
+	server := app.InitApp(app, config)
+
+	fmt.Printf("[Starting server in localhost%v...]", config.Addr)
 	server.ListenAndServe()
 }
--- setup.go
+++ setup.go
@@ -1,5 +1,24 @@
 package main
 
+import (
+	"net/http"
+
+	"github.com/gorilla/mux"
+)
+
 type App struct{}
 
-func (a *App) InitServer() {}
+type AppConfig struct {
+	Addr string
+}
+
+func (a *App) InitApp(app App, config AppConfig) *http.Server {
+	r := mux.NewRouter()
+	app.initRoutes(r)
+
+	server := &http.Server{
+		Addr:    config.Addr,
+		Handler: r,
+	}
+	return server
+}
